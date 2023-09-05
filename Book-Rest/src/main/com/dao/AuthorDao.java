7a62fa51815472a8f88801fe2c46619ca39ffcb2
Abhinav Jha <97444941+abhinav3254@users.noreply.github.com>
2023-09-05 18:28:53 +0530

    Delete Book-Rest/src/main/com directory

--- Book-Rest/src/main/com/dao/AuthorDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pojo.Author;
-
-public interface AuthorDao extends JpaRepository<Author, Integer> {
-
-}
--- Book-Rest/src/main/com/dao/PublishersDao.java
+++
@@ -1,9 +0,0 @@
-package com.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pojo.Publishers;
-
-public interface PublishersDao extends JpaRepository<Publishers, Integer> {
-
-}
